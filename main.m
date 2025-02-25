1a672d3f0d9232afaba1c49ae0bb301ad95c3858
Michael Corelli <michaelcorelli11@gmail.com>
2025-02-25 22:22:33 +0100

    Created calibration.m and main.m files with loading of the dataset.

---
+++ main.m
@@ -0,0 +1,28 @@
+close all
+clear
+clc
+
+source "calibration.m"
+
+h = figure(1);
+more off;
+
+#parameters: Ksteer, Ktraction, axis_length and steer_offset
+Ksteer = 0.1;
+Ktraction = 0.0106141;
+axis_length = 1.4;
+steer_offset = 0;
+
+#load the dataset and split the data in variables: time, ticks, model_pose and tracker_pose
+disp('Loading the dataset');
+
+file = fopen('./dataset.txt', 'r');
+Z = textscan(file, 'time: %f ticks: %f %f model_pose: %f %f %f tracker_pose: %f %f %f', 'MultipleDelimsAsOne', true, 'HeaderLines', 8);
+fclose(file);
+
+time = Z{1};
+ticks = [Z{2}, Z{3}];
+model_pose = [Z{4}, Z{5}, Z{6}];
+tracker_pose = [Z{7}, Z{8}, Z{9}];
+
+disp('Dataset loaded');
